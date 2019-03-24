8bcf1ce22a667c24c34a408c70ca23792ce4161b
Nikita Vilunov <nikitaoryol@gmail.com>
2019-03-24 11:30:00 +0300

    get track endpoint

--- flask/app.py
+++ flask/app.py
@@ -10,17 +10,21 @@ def balance(address):
     return j({"address": address, "balance": balance(address)})
 
 
-@app.route("/track/<address>/", methods=["POST"])
+@app.route("/track/<address>/", methods=["GET", "POST"])
 def track(address):
-    assert request.method == "POST"
-    data = request.json
-    assert isinstance(data, list)
-    for i in data:
-        assert isinstance(i.get("timestamp"), int)
-        assert isinstance(i.get("latitude"), float)
-        assert isinstance(i.get("longitude"), float)
-    send_track(address, data)
-    return "succses", 200
+    if request.method == "GET":
+        return j(get_track(address))
+    elif request.method == "POST":
+        data = request.json
+        print(data)
+        assert isinstance(data, list)
+        for i in data:
+            assert isinstance(i.get("timestamp"), int)
+            assert isinstance(i.get("latitude"), float)
+            assert isinstance(i.get("longitude"), float)
+        send_track(address, data)
+        return j("success")
+    raise Exception("Invalid method")
 
 
 
--- flask/blockchain.py
+++ flask/blockchain.py
@@ -5,7 +5,7 @@ from web3 import eth as e, Web3, HTTPProvider
 from web3.utils.encoding import to_int
 from web3.middleware import geth_poa_middleware
 
-__all__ = ("balance", "send_track")
+__all__ = ("balance", "send_track", "get_track")
 
 
 node1 = HTTPProvider("http://192.168.0.2:8501")
@@ -32,7 +32,9 @@ contract_addr = w3.toChecksumAddress(contract_addr)
 def get_contract(addr):
     return eth.contract(address=contract_addr, abi=abi)
 
+
 contract = get_contract(contract_addr)
+
 
 def balance(address):
     return eth.getBalance(w3.toChecksumAddress(address))
@@ -52,3 +54,14 @@ def send_track(address, track):
     for c in calls:
         c.transact()
 
+
+def get_track(address):
+    address = w3.toChecksumAddress(address)
+    len = contract.functions.get_trace_length(address).call()
+    return [
+        dict(longitude=longitude / precision, latitude=latitude / precision, timestamp=timestamp)
+        for longitude, latitude, timestamp in (
+            contract.functions.get_trace_point(address, i).call()
+            for i in range(len)
+        )
+    ]
